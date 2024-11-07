e8b3f59face9811bc3f19a18d0b745918654bf81
Adam Demasi <adam@hbang.com.au>
2024-11-07 23:28:07 +1030

    Implement pausable, resumable, and seekable Lottie animations
    Co-authored-by: Jacob Clayden <me@jacobcx.dev>

--- src/Avalonia.Labs.Lottie/LottiePayload.cs
+++ src/Avalonia.Labs.Lottie/LottiePayload.cs
@@ -7,4 +7,8 @@ internal record struct LottiePayload(
     SkiaSharp.Skottie.Animation? Animation = null,
     Stretch? Stretch = null,
     StretchDirection? StretchDirection = null,
-    int? RepeatCount = null);
+    int? RepeatCount = null,
+    float? SeekFrame = null,
+    float? SeekProgress = null,
+    Action? OnAnimationCompleted = null,
+    Action<int>? OnAnimationCompletedRepetition = null);
